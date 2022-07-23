0cf90e066ea97984bc889055b9bb456042974cba
Aaron Noe <aarminoe@gmail.com>
2022-07-23 09:01:55 -0500

    commit tree 54fff13aca08273fc1d95e627ed7d8e36e86cca7

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,50 @@
 require_relative './node'
 
 class LinkedList
+    attr_accessor :head
+    
+    def initialize
+        @head = nil
+    end
+
+    def prepend(node)
+        if head.nil?
+            self.head = node
+            return
+        else
+            self.head = node
+        end
+    end
+
+    def append(node)
+        if head.nil? 
+            self.head = node
+        else
+            last_node = head
+            while last_node.next_node
+                last_node = last_node.next_node 
+            end
+
+            last_node.next_node = node
+        end
+    end
+
+    def delete_head
+        if head.nil? 
+          
+        elsif head && head.next_node = false
+        else
+        end
+    end
+
+    def delete_tail
+        if head.nil?
+            return
+        else
+            self.data.pop
+        end
+
+    end
+
 
 end
